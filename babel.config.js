153a686a34fa58438a0874bd120aca2ec8f35f23
seonpark <pksung1@gmail.com>
2021-10-03 16:42:51 +0900

    feat. set babel alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,34 @@
+console.log('BABEL CONFIG JSON');
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@app': ['./app'],
+          '@components': ['./app/components'],
+          '@containers': ['./app/containers'],
+          '@navigator': ['./app/navigator'],
+          '@pages': ['./app/pages'],
+          '@store': ['./app/store'],
+          '@theme': ['./app/theme'],
+          '@utils': ['./app/utils'],
+          '@hooks': ['./app/hooks'],
+        },
+      },
+    ],
+  ],
 };
